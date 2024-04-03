796ee6c91dc5f8a3c5b6af1fd8fcb86301181316
jeannoj99 <jeannotjynaldo@gmail.com>
2024-04-03 16:01:41 +0200

    update target

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,19 @@
+import org.apache.spark.sql.{DataFrame,SparkSession}
+import org.apache.spark.SparkConf
+
 object Main extends App {
-  println("Hello, World!")
+  val conf: SparkConf = new SparkConf()
+  conf.set("spark.driver.memory","1G")
+    .set("spark.testing.memory", "2147480000")
+
+  val sparkSession = SparkSession
+    .builder()
+    .master("local[1]")
+    .config(conf)
+    .enableHiveSupport()
+    .getOrCreate()
+
+  val inputDF = sparkSession.sql("SELECT 'A' ")
+    inputDF.show()
 }
+
